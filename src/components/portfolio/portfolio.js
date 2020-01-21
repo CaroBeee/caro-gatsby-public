afa7af6e632629423d5ffd11f23a76769df686ad
Caro B <web@devcarol.de>
2020-01-21 21:58:27 +0100

    added window fail- config

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -2,6 +2,16 @@ import React, { Component } from "react"
 import PortfolioItem from "./portfolio-item"
 import bulmaCarousel from "../../../node_modules/bulma-carousel/dist/js/bulma-carousel.min.js"
 
+// Requiring function causes error during builds
+// as the code tries to reference window
+const module = require("module") // Error
+// Wrap the require in check for window
+if (typeof window !== `undefined`) {
+  const module = require("module")
+}
+
+const module = typeof window !== `undefined` ? require("module") : null
+
 class Portfolio extends Component {
   constructor(props) {
     super(props)
